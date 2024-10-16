0e98c7114e4cc047a41b0182829c47bc01fc10e0
ismacl <icabanasl@fpcoruna.afundacion.org>
2024-10-16 13:57:47 +0200

    Ejercicio 2 PHP

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<h1>Pagina de bienvenida</h1>
+		<?php
+		function dar_bienvenida($nombre) {
+		echo "¡Bienvenido/a, " .$nombre . "!";
+}
+
+		dar_bienvenida("Bart Simpson");
+		?>
+	</body>
+</html>
